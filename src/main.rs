ff9172e3f09981596edfe67b02700cf15ff6881d
k4-100 <kmakingk@gmail.com>
2024-10-21 21:48:35 +0200

    initial

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        // .add_systems(Startup, ())
+        // .add_systems(Update, ())
+        .run();
+}
