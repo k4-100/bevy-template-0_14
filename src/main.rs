75b794f04db5376cc13ea6980ab96c3295d35fa7
k4-100 <kmakingk@gmail.com>
2024-10-22 23:08:30 +0200

    barebone setup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use bevy::prelude::*;
 
+pub mod plugins;
+pub mod utils;
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((DefaultPlugins, plugins::game::GamePlugin))
         // .add_systems(Startup, ())
         // .add_systems(Update, ())
         .run();
